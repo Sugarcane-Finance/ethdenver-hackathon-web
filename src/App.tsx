5021f778e53d583ef1fe528dc4b68d39dd2e8c6c
Angel Batista <angel.batista1992@gmail.com>
2023-02-24 18:24:56 -0700

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TestPage from "./pages/test/Test.page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/test",
+    element: <TestPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">hello</header>
       <Button variant="contained">Hello World</Button>
+
+      <Box>Pages:</Box>
+
+      <RouterProvider router={router} />
     </div>
   );
 }
